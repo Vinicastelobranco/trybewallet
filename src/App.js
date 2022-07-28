491f549513464b3cebebc0e9569b3dbe79084ac6
Vinicius Castelo Branco <vinicastelobranco@gmail.com>
2022-07-28 16:04:20 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -3,16 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route path="/" component={ Login } exact />
-          <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
-        </Switch>
-
-      </div>);
-  }
+function App() {
+  return <div>Hello, TrybeWallet!</div>;
 }
+// inicio
 export default App;
