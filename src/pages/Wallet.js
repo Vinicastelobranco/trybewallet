71bd14388bfb4908f1fd79327f1ded649880fc2f
Vinicius Castelo Branco <vinicastelobranco@gmail.com>
2022-08-03 15:43:19 -0300

    Requisito 2

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
-import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <main>
-        <header>
-          <div>
-            <Header />
-          </div>
-        </header>
-        <section>
-          <div>
-            <WalletForm />
-          </div>
-        </section>
-      </main>);
+      <div>
+        <Header />
+      </div>);
   }
 }
 
